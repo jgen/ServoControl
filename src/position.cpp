39a8a933aea2e2b1e20707b6484f685bf520da6d
Adam Wilson <slauter6@gmail.com>
2012-05-19 11:12:53 -0400

    Changed the data members in position to better suit the data that is beign stored, added the toString method for position class

--- src/position.cpp
+++ src/position.cpp
@@ -1,12 +1,33 @@
 #include "position.h"
 
 Position::Position(QObject *parent) :
-    QObject(parent)
+    QObject(parent),
+    m_PWMRepeatMap(),
+    m_data(),
+    m_hasData(false),
+    m_isFreeze(false),
+    m_hasDelay(false),
+    m_hasPWM(false)
 {
     init();
 }
 void Position::init()
 {
+    m_PWMRepeatMap.insert(0,1);
+    m_PWMRepeatMap.insert(1,2);
+    m_PWMRepeatMap.insert(2,10);
+    m_PWMRepeatMap.insert(3,25);
+    m_PWMRepeatMap.insert(4,50);
+    m_PWMRepeatMap.insert(5,100);
+    m_PWMRepeatMap.insert(6,150);
+    m_PWMRepeatMap.insert(7,200);
+
+    for(quint8 i = 1; i < 16; i++)
+    {
+        quint8 d = 2;
+        m_data.insert(i,d);
+    }
+    qDebug() << this->toString();
 }
 
 /*Public Methods*/
@@ -14,6 +35,27 @@ bool Position::fromString(QString fileFormatString)
 {
 }
 QString Position::toString()
+{
+    QString output = createStartOfString();
+    for(int i = 1; i <= 12; i++)
+    {
+        if(m_data.contains(i))
+        {
+            this->addTerminatingComma(output);
+            output.append(QString("%1,%2").arg(i,3,10,QLatin1Char('0'))
+                          .arg(m_data.value(i),3,10,QLatin1Char('0')));
+        }
+    }
+    if(m_data.contains(Position::SeqDelay))
+    {
+        this->addTerminatingComma(output);
+        output.append(QString("SeqDelay,%1").arg(m_data.value(SeqDelay),3,10,QLatin1Char('0')));
+    }
+    return output;
+
+}
+
+QString Position::createStartOfString()
 {
     QString output = "";
     if (m_isFreeze)
@@ -27,20 +69,25 @@ QString Position::toString()
     if (m_data.contains(PWMRepeat))
     {
         output.append("PWMRep,");
-        output.append(m_data.value(PWMRepeat));
-        output.append(",");
+        output.append(QString("%1").arg(m_PWMRepeatMap.value(m_data.value(PWMRepeat)),3,10,QLatin1Char('0')));
     }
     if (m_data.contains(PWMSweep))
     {
-        if (!output.endsWith(",") || !output.endsWith("*") || !output.endsWith("&"))
-        {
-            output.append(",");
-        }
+        this->addTerminatingComma(output);
         output.append("PWMSweep,");
-        output.append(m_data.value(PWMSweep));
-        output.append(",");
+        output.append(QString("%1").arg(m_data.value(PWMSweep),3,10,QLatin1Char('0')));
+    }
+    return output;
+}
+
+void Position::addTerminatingComma(QString& string)
+{
+    if (!string.endsWith(",") || !string.endsWith("*") || !string.endsWith("&"))
+    {
+        string.append(",");
     }
 }
+
 QByteArray Position::toSerialData()
 {
 
--- src/position.h
+++ src/position.h
@@ -6,11 +6,16 @@
 #include <QStringList>
 #include <QByteArray>
 #include <QMap>
+#include <QDebug>
+
+/*
+ Storage Notes: Store PWMRepeat and SequenceReplay as 0-->15, use lookup for actual value.
+*/
 class Position : public QObject
 {
     Q_OBJECT
 public:
-    enum SpecialFunction {PWMRepeat = 13, PWMSweep, SeqDelay};
+    enum SpecialFunction {PWMRepeat = 13, PWMSweep = 14, SeqDelay = 15};
     explicit Position(QObject *parent = 0);
 
 
@@ -23,8 +28,15 @@ public slots:
 
 private:
     void init();
+    void addTerminatingComma(QString& string);
+    QString createStartOfString();
+
+    QMap<quint8,int> m_PWMRepeatMap;
     QMap<quint8,quint8> m_data;
+    bool m_hasData;
     bool m_isFreeze;
+    bool m_hasDelay;
+    bool m_hasPWM;
 
 };
 
