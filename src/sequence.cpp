36a2bb03cb585676db4b5eea5bfc8dd2e020c636
Adam Wilson <slauter6@gmail.com>
2012-05-22 18:56:57 -0400

    Added format comments, started parsing of header of file from string

--- src/sequence.cpp
+++ src/sequence.cpp
@@ -2,8 +2,28 @@
 
 Sequence::Sequence(QObject *parent) :
     QObject(parent),
-    m_positions()
+    m_positions(),
+    m_sequenceReplay(0),
+    m_PWMRepeat(0),
+    m_PWMSweep(0),
+    m_sequenceDelay(0),
+    m_runFormat(0),
+    m_databank(0),
+    m_replayMap(),
+    m_comments()
 {
+}
+
+void Sequence::init()
+{
+    m_replayMap.insert(0,1);
+    m_replayMap.insert(1,2);
+    m_replayMap.insert(2,10);
+    m_replayMap.insert(3,25);
+    m_replayMap.insert(4,50);
+    m_replayMap.insert(5,100);
+    m_replayMap.insert(6,150);
+    m_replayMap.insert(7,200);
 }
 
 /*Public Methods*/
@@ -14,7 +34,74 @@ QString Sequence::toString()
 
 bool Sequence::fromString(const QString data)
 {
+    QTextStream stream(&data,QIODevice::ReadOnly);
+    int lineNumber = 0;
+    while(stream.atEnd())
+    {
+        lineNumber++;
+        QString line(stream.readLine());
+        if(line.startsWith('#'))//Comment line
+        {
+            m_comments.insert(lineNumber,line);
+            continue;
+        }
+        else if(line.startsWith('0'))//Only line that should to this is the first
+        {                           //it cannot start with anything else
+            if (!this->parseFileHeader(line))
+            {
+                return false; //failed parsing the header.
+            }
+            continue;
+        }
 
+    }
+}
+
+bool Sequence::parseFileHeader(QString &header)
+{
+    QStringList data = header.split(',',QString::SkipEmptyParts);
+    if (data.size() != 6)
+    {
+        return false;//Header is not in the right format.
+    }
+    if (!this->ParseRangeStore(data.at(0),m_runFormat,0,4))
+    {
+        return false;//Wrong format for runFormat
+    }
+    if (!this->ParseRangeStore(data.at(1),m_databank,0,5))
+    {
+        return false; //Wrong format for databank.
+    }
+    if (!this->ParseRangeStore(data.at(2),m_PWMRepeat,0,7))
+    {
+        return false; //Wrong PWM repeat format
+    }
+    if (!this->ParseRangeStore(data.at(3),m_PWMSweep,0,15))
+    {
+        qDebug() << tr("Header: PWM sweep value what is the wrong format");
+        return false;
+    }
+    if (!this->ParseRangeStore(data.at(4),m_sequenceDelay,0,15))
+    {
+        qDebug() << tr("Header: Sequence Delay is in the wrong format");
+        return false;
+    }
+    if (!this->ParseRangeStore(data.at(5),m_sequenceReplay,0,7))
+    {
+        qDebug() << tr("Header: Sequence replay is in the wrong format");
+    }
+    return true;
+}
+inline bool Sequence::ParseRangeStore(QString& source, quint8& dest, quint8 min, quint8 max)
+{
+    bool ok = false;
+    quint8 temp = source.toUShort(ok,10);
+    if (!ok || temp < min || temp > max)
+    {
+        return false;
+    }
+    dest = temp;
+    return true;
 }
 
 bool Sequence::toFile(QFile &outputFile)
@@ -25,5 +112,10 @@ bool Sequence::toFile(QFile &outputFile)
 bool Sequence::fromFile(QFile &intputFile)
 {
 
+}
+
+void Sequence::addPosition(Position newPosition)
+{
+    this->m_positions.append(newPosition);//We need a copy constructor for positions.
 }
 
--- src/sequence.h
+++ src/sequence.h
@@ -17,6 +17,8 @@ public:
     bool toFile(QFile& outputFile);
     bool fromFile(QFile& intputFile);
 
+    void addPosition(Position newPosition);
+
     //TODO: Add stream operators for string and file stream.
 
 signals:
@@ -24,7 +26,69 @@ signals:
 public slots:
 
 private:
+    void init();
+    bool parseFileHeader(QString& header);
+
     Positions m_positions;
+    quint8 m_sequenceReplay;//Stores the value to be sent to serial port(key)
+    /* RS: Number of times a sequence is repeated. (value)
+       LS: Number sent to the serveo (key)
+    FFF = Sequence Replay
+            0=======1
+            1=======2
+            2=======10
+            3=======25
+            4=======50
+            5=======100
+            6=======150
+            7=======200
+    */
+
+    quint8 m_PWMRepeat;
+    /*
+        CCC = PWM Repeat
+        RS: Number of times a signal is sent to a servo, before the sweep is added to it.
+        (RS * 8ms = time servo sits still between sweep increments)
+        LS: Value sent  to the board to get the RS delay.
+        0=======1
+        1=======2
+        2=======10
+        3=======25
+        4=======50
+        5=======100
+        6=======150
+        7=======200
+    */
+
+    quint8 m_PWMSweep;
+    /*
+    DDD = PWM Sweep - Inc/Dec value for PWM during sweep to target.(0 = no sweep)
+            000 -> 015
+    */
+
+    quint8 m_sequenceDelay;
+    /*
+    EEE = Sequence Delay - Time between positions being sent
+            000 -> 015
+    */
+
+    quint8 m_runFormat;
+    /*
+     AAA = Run format //Ignored, can be used to denote formatting but not needed.
+             0=======Direct Run
+             1=======Sequence 12
+             2=======Sequence 3
+             3=======Sequence 1
+             4=======Freeze
+   */
+
+    quint8 m_databank;
+    /*
+    BBB = Data Bank //Unneeded, can be ignored
+        000 -> 005
+    */
+    QMap<quint8,int> m_replayMap; //Index = key, Replays= value;
+    QMap<int,QString> m_comments; //Line Number Comment from = key , comment = value
 
 };
 
