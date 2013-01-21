8f087644c334f5eda5bab0ee16a7e22c9216ffdb
Adam Wilson <awils49@uwo.ca>
2013-01-21 17:12:38 -0500

    More sequence tests

--- tst/serial_app_tst/tst_sequence.h
+++ tst/serial_app_tst/tst_sequence.h
@@ -27,6 +27,11 @@ private slots:
     void testAddStartPositionInvalid();
     void testAddStartPositionInvalid_data();
 
+    //From text - along with every combination of junk.
+    //from and to string are inverses
+
+    //Check that the iterator works, and that it moves/resets correctly
+
 
 
 private:
